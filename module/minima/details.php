1bb762a458379161551f58986ae89ce896303de4
pasyuk <pasyuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-01 13:25:43 +0000

    * Fix for modern themes compatibility (Music player not worked)
    git-svn-id: https://plugins.svn.wordpress.org/grand-media/tags/0.9.3@780884 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ module/minima/details.php
@@ -0,0 +1,18 @@
+<?php
+$module = array(
+	'title'        => 'Minima',
+	'version'      => '1.2',
+	'author'       => 'CodEasily.com',
+	'description'  => 'Multi-categorized professional image gallery skin with slideshow feature. This is the free light version of <a target="_blank" href="http://codeasily.com/portfolio-item/gmedia-optima/">Optima Module</a>.',
+	'features'     => '',
+	'type'         => 'gallery',
+	'status'       => 'free',
+	'uid'          => 'gmMinima',
+	'demo'         => '',
+	'dependencies' => 'swfobject'
+);
+
+
+
+
+
